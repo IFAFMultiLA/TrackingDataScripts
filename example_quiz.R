25398827b8619690b46d86f391790154577a4115
Markus Konrad <post@mkonrad.net>
2023-06-12 15:27:17 +0200

    WIP

--- example_quiz.R
+++ example_quiz.R
@@ -1,10 +1,12 @@
 library(dplyr)
 library(jsonlite)
+library(ggplot2)
 
-sess <- read.csv('data/2023-06-12_133039_476bc20dbf/tracking_sessions.csv')
-events <- read.csv('data/2023-06-12_133039_476bc20dbf/tracking_events.csv')
 
-quizdata <- left_join(sess, events, by = c('track_sess_id')) %>%
+sess <- read.csv('data/tracking_sessions.csv')
+events <- read.csv('data/tracking_events.csv')
+
+quizdata <- left_join(sess, events, by = c('track_sess_id')) |>
     filter(event_type == "learnr_event_question_submission")
 
 head(quizdata)
@@ -13,3 +15,37 @@ events_parsed <- lapply(quizdata$event_value, fromJSON)
 
 quizdata$quiz_label <- sapply(events_parsed, function(quizevent) { quizevent$label })
 quizdata$answ_correct <- sapply(events_parsed, function(quizevent) { quizevent$correct })
+
+quizdata <- select(quizdata, track_sess_id, track_sess_start, track_sess_end, event_time, quiz_label, answ_correct) |>
+    mutate(quiz_label = as.factor(quiz_label),
+           track_sess_start = as.POSIXct(gsub("T", " ", track_sess_start)),
+           track_sess_end = as.POSIXct(gsub("T", " ", track_sess_end)),
+           event_time = as.POSIXct(gsub("T", " ", event_time)))
+
+head(quizdata)
+
+quiztries <- group_by(quizdata, track_sess_id, quiz_label) |>
+    arrange(track_sess_id, quiz_label, event_time) |>
+    mutate(quiz_try = row_number()) |>
+    filter(answ_correct) |>
+    select(-answ_correct) |>
+    ungroup()
+
+head(quiztries)
+
+tries_per_label <- select(quiztries, quiz_label, quiz_try) |>
+    group_by(quiz_label) |>
+    summarise(n = n(),
+              mean_tries = mean(quiz_try),
+              median_tries = median(quiz_try),
+              sd_tries = sd(quiz_try),
+              se_tries = sd(quiz_try) / sqrt(n)) |>
+    ungroup()
+
+
+head(tries_per_label)
+
+ggplot(tries_per_label, aes(quiz_label, mean_tries)) +
+    geom_pointrange(aes(ymin = mean_tries - sd_tries, ymax = mean_tries + sd_tries)) +
+    theme_minimal() +
+    labs(title = "Anzahl Versuche bis zur Lösung pro Quiz", x = "Quiz", y = "Versuche")
